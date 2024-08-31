1aa2d9f9eb1bafc0079bd2c8f63feeab51891cdf
InfoSecJay <j.s.tymchuk@gmail.com>
2024-08-31 15:04:07 -0400

    Update folder structure and add files

---
+++ Lectures/Lecture 0/calculator.py
@@ -0,0 +1,10 @@
+#x = int(input("whats x? "))
+#y = int(input("whats y? "))
+
+x = float(input("whats x? "))
+y = float(input("whats y? "))
+
+z = round(x + y)
+
+# add colon and comma to split the numbers with commas
+print(f"{z:,}")
