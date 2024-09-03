c0bc4a123b0d87e95c16058150705311b7fa0ed6
InfoSecJay <j.s.tymchuk@gmail.com>
2024-09-03 16:55:03 -0400

    lecture 2 assignments

---
+++ Lectures/Lecture 2/assignments/camel/camel.py
@@ -0,0 +1,19 @@
+"""
+In a file called camel.py, implement a program that prompts the user for the name of a variable in camel case and outputs the corresponding name in snake case.
+Assume that the user’s input will indeed be in camel case.
+"""
+
+def main():
+     camel_case = input("CamelCase: ")
+     return snake_case(camel_case)
+
+def snake_case(camel_case):
+    snake_case = ""
+    for i in range(len(camel_case)):
+        if camel_case[i].isupper():
+            snake_case = snake_case + "_" + camel_case[i].lower()
+        else:
+            snake_case += camel_case[i]
+    print("snake case: ", snake_case)
+            
+main()
